caa18e338ff75427fcb9a4400998ac57f89fa019
David Martinho <davidmartinho@gmail.com>
2013-02-26 19:41:16 +0000

    First version of the coffee-manager application after integrating it with bennu-backbone and marionette.js

---
+++ src/main/webapp/coffee-manager/js/nls/Header.js
@@ -0,0 +1,11 @@
+define({
+    "root": {
+        "Coffee Manager": "Coffee Manager",
+        "New Order": "New Order",
+        "List Batches": "List Batches",
+        "My Orders": "My Orders",
+        "Logout": "Logout"
+    },
+    "pt-pt": true,
+    "fr-fr": true
+});
